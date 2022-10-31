9bc352dac57287515f18862f031706fd1305d420
tiziberto <101434963+tiziberto@users.noreply.github.com>
2022-10-30 22:14:38 -0300

    Update trabajo.cpp

--- 00 TRABAJO FINAL/trabajo.cpp
+++ 00 TRABAJO FINAL/trabajo.cpp
@@ -1,29 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
 
 using namespace std;
 
-class Lectura
+class cityclass
 {
     private:
-        struct city;
+        struct timestamp;
+		struct measurement;
+		struct city;
     public:
         void LeerDatos(FILE);
-};
-
-struct city /*Estructura principal*/
-{
-struct city* next;
-struct measurement; 
-int cityId;
-char cityname [50]; 
-};
-
-struct measurement 
-{
-float temp; 
-float hum;
-struct timestamp; 
 };
 
 struct timestamp 
@@ -34,12 +22,32 @@ int hh;
 int mm;
 };
 
+struct measurement 
+{
+float temperatura; 
+float hum;
+struct timestamp fecha; 
+};
+
+struct city /*Estructura principal*/
+{
+struct city* next;
+struct measurement temperaturas; 
+int cityId;
+char cityname [50]; 
+};
+
 int main(){
+    struct city* head=NULL;
+	struct city* temp= NULL;
+	struct city* new_node=NULL;
     struct city ciudad;
     struct measurement temps;
     struct timestamp tiempo;
     int provincia=0;
     char data=0;
+    char nombreciudad[50];
+    string nombreciudadstring;
     FILE *fp;
     fp=fopen("./data_set.txt", "r");
     if (fp==NULL)
@@ -49,9 +57,33 @@ int main(){
     }
     while (!feof(fp))
     {
-        fscanf(fp, "%d %d %s %f %f %d %d %d %d", &ciudad.cityId, &provincia, &ciudad.cityname, &temps.temp, &temps.hum, &tiempo.hh, &tiempo.mm, &tiempo.day, &tiempo.month);
-        printf("%s\n", ciudad.cityname);
+        fscanf(fp, "%d %d %s %f %f %d %d %d %d", &ciudad.cityId, &provincia, &nombreciudadstring, &temps.temperatura, &temps.hum, &tiempo.hh, &tiempo.mm, &tiempo.day, &tiempo.month);
+        new_node = (struct city*)malloc(sizeof(struct city)+sizeof(struct measurement)+sizeof(struct timestamp));
+		if(new_node==NULL){
+			printf("No hay memoria disponible");
+			exit(1);
+		}
+		new_node->cityId=ciudad.cityId;/*Declaracion de cityID en el nuevo nodo*/
+		new_node->temperaturas.temperatura=temps.temperatura;/*Declaracion de temperatura en el nuevo nodo*/
+		new_node->temperaturas.hum=temps.hum;/*Declaracion de humedad en el nuevo nodo*/
+		new_node->temperaturas.fecha.month=tiempo.month;/*Declaracion del mes en el nuevo nodo*/
+		new_node->temperaturas.fecha.day=tiempo.day;/*Declaracion del dia en el nuevo nodo*/
+		new_node->temperaturas.fecha.hh=tiempo.hh;/*Declaracion de la hora en el nuevo nodo*/
+		new_node->temperaturas.fecha.mm=tiempo.mm;/*Declaracion de minutos en el nuevo nodo*/
+		new_node->next=NULL;/*Si la lista esta vacia, el nodo ingresado es el primero de la lista*/
+		if(head==NULL){
+			head=new_node;
+		}else{
+			temp=head;
+			while(temp->next!=NULL){
+				temp=temp->next;
+			}
+			temp->next=new_node;
+		}
     }
+    if(head==NULL){
+		printf("Lista vacia\n");
+		exit(1);
+	}
     return 0;
 }
-
